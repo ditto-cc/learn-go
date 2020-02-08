ccc60c96ae17d22de4da92c1fbc6990ae324cd28
cc <chengchen.chn@outlook.com>
2020-02-09 00:36:20 +0800

    WIP:update 1261 leetcode

---
+++ leetcode/1261-find-elements-in-a-contaminated-binary-tree.go
@@ -0,0 +1,110 @@
+package leetcode
+
+import "strconv"
+
+/**
+Given a binary tree with the following rules:
+
+root.val == 0
+If treeNode.val == x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+If treeNode.val == x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+
+You need to first recover the binary tree and then implement the FindElements class:
+
+FindElements(TreeNode* root) Initializes the object with a contamined binary tree, you need to recover it first.
+bool find(int target) Return if the target value exists in the recovered binary tree.
+
+
+Example 1:
+
+
+
+Input
+["FindElements","find","find"]
+[[[-1,null,-1]],[1],[2]]
+Output
+[null,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1]);
+findElements.find(1); // return False
+findElements.find(2); // return True
+Example 2:
+
+
+
+Input
+["FindElements","find","find","find"]
+[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+Output
+[null,true,true,false]
+Explanation
+FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+findElements.find(1); // return True
+findElements.find(3); // return True
+findElements.find(5); // return False
+Example 3:
+
+
+
+Input
+["FindElements","find","find","find","find"]
+[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+Output
+[null,true,false,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+findElements.find(2); // return True
+findElements.find(3); // return False
+findElements.find(4); // return False
+findElements.find(5); // return True
+
+
+Constraints:
+
+TreeNode.val == -1
+The height of the binary tree is less than or equal to 20
+The total number of nodes is between [1, 10^4]
+Total calls of find() is between [1, 10^4]
+0 <= target <= 10^6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-elements-in-a-contaminated-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type FindElements struct {
+	root *TreeNode
+}
+
+func Constructor(root *TreeNode) FindElements {
+	return FindElements{root: root}
+}
+
+func (this *FindElements) Find(target int) bool {
+	val := (1 >> (len(strconv.FormatInt(int64(target+1), 2)) - 1)) >> 1
+	node := this.root
+	for val > 0 && node != nil {
+		if val&target == 0 {
+			node = node.Left
+		} else {
+			node = node.Right
+		}
+		val >>= 1
+	}
+	return node != nil
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Find(target);
+ */
