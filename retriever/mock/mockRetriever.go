9773df052191b523312440af056dd4aed929a99c
cc <chengchen.chn@outlook.com>
2020-02-10 20:27:03 +0800

    WIP:update interface

--- retriever/mock/mockRetriever.go
+++ retriever/mock/mockRetriever.go
@@ -1,9 +1,20 @@
 package mock
 
+import "fmt"
+
 type Retriever struct {
 	Contents string
 }
 
+func (r *Retriever) String() string {
+	return fmt.Sprintf("Retriever: {contents=%s}", r.Contents)
+}
+
 func (r *Retriever) Get(url string) string {
+	return r.Contents
+}
+
+func (r *Retriever) Post(url string, m map[string]string) string {
+	r.Contents = m["contents"]
 	return r.Contents
 }
--- retriever/retriever.go
+++ retriever/retriever.go
@@ -7,16 +7,43 @@ import (
 	"time"
 )
 
+const url string = "www.baidu.com"
+
 type Retriever interface {
 	Get(url string) string
+}
+
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+type Session interface {
+	Retriever
+	Poster
 }
 
 func download(r Retriever) string {
 	return r.Get("http://www.baidu.com")
 }
 
+func post(p Poster) {
+	p.Post("www.baidu.com", map[string]string{
+		"contents": "12345",
+	})
+}
+
+func session(s Session) string {
+	fmt.Println("Trying Session.")
+	s.Post(url, map[string]string{
+		"contents": "another faked baidu.com",
+	})
+	return s.Get(url)
+}
+
+
 func inspect(r Retriever) {
-	fmt.Printf("%T %v\n", r, r)
+	fmt.Println("Inspecting", r)
+	fmt.Printf("> %T %v\n> Type switch: ", r, r)
 	switch v := r.(type) {
 	case *mock.Retriever:
 		fmt.Println("Contents:", v.Contents)
@@ -27,15 +54,20 @@ func inspect(r Retriever) {
 
 func main() {
 	var r Retriever
-	r = &mock.Retriever{"this is a mock retriever."}
+	retriever := &mock.Retriever{"this is a mock retriever."}
+	r = retriever
 	inspect(r)
+	fmt.Println()
+	fmt.Println(download(retriever))
+	fmt.Println(session(retriever))
+	fmt.Println()
 	r = &real.Retriever{UserAgent: "Mozilla/5.0", Timeout: time.Minute}
 	inspect(r)
+	fmt.Println()
 	// Type Assertion
 	if realRetriever, ok := r.(*real.Retriever); ok {
 		fmt.Println(realRetriever.Timeout)
 	} else {
 		fmt.Println("not *real retriever.")
 	}
-	//fmt.Println(download(r))
 }
