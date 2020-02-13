a0cb7b7c1da8916c7893258ab8cd5113b9f5df1f
cc <chengchen.chn@outlook.com>
2020-02-13 11:54:32 +0800

    WIP: add leetcode 1295

---
+++ leetcode/1295-find-numbers-with-even-number-of-digits.go
@@ -0,0 +1,52 @@
+package leetcode
+
+/**
+Given an array nums of integers, return how many of them contain an even number of digits.
+ 
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation:
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits.
+Example 2:
+
+Input: nums = [555,901,482,1771]
+Output: 1
+Explanation:
+Only 1771 contains an even number of digits.
+ 
+
+Constraints:
+
+1 <= nums.length <= 500
+1 <= nums[i] <= 10^5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-numbers-with-even-number-of-digits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func getDigits(num int) int {
+	d := 1
+	for ; num >= 10; num /= 10 {
+		d++
+	}
+	return d
+}
+
+func findNumbers(nums []int) int {
+	res := 0
+	for _, e := range nums {
+		if getDigits(e) & 1 == 0 {
+			res++
+		}
+	}
+	return res
+}
