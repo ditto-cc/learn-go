8bd3c7c29c067658b601a12021d06fe5b5c9925b
cc <chengchen.chn@outlook.com>
2020-02-17 23:17:21 +0800

    WIP: update leetcode 771

--- leetcode/0771-jewels-and-stones.go
+++ leetcode/0771-jewels-and-stones.go
@@ -1,1 +1,38 @@
 package leetcode
+
+/**
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/jewels-and-stones
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func numJewelsInStones(J string, S string) int {
+	set := map[rune]int{}
+	res := 0
+	for _, c := range J {
+		set[c] = 0
+	}
+	for _, c := range S {
+		if _, ok := set[c]; ok {
+			res ++
+		}
+	}
+	return res
+}
