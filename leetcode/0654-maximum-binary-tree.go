33a9016d449eda3eb883abb745f2edfbe6750150
cc <chengchen.chn@outlook.com>
2020-02-17 23:17:39 +0800

    WIP: update leetcode 654

--- leetcode/0654-maximum-binary-tree.go
+++ leetcode/0654-maximum-binary-tree.go
@@ -1,1 +1,41 @@
 package leetcode
+
+/**
+Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
+
+The root is the maximum number in the array.
+The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+Construct the maximum tree by the given array and output the root node of this tree.
+
+Example 1:
+Input: [3,2,1,6,0,5]
+Output: return the tree root node representing the following tree:
+
+      6
+    /   \
+   3     5
+    \    /
+     2  0
+       \
+        1
+Note:
+The size of the given array will be in the range [1,1000].
+通过次数10,422提交次数13,193
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+
+}
