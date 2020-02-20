61e3e7acac0ad2f6b7574c9b766efb2ba83f61b7
cc <chengchen.chn@outlook.com>
2020-02-20 14:38:14 +0800

    WIP: update leetcode 101

---
+++ leetcode/0101-symmetric-tree.go
@@ -0,0 +1,52 @@
+package leetcode
+
+/**
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSymmetricNode(node1, node2 *TreeNode) bool {
+	if node1 == nil && node2 == nil { return true }
+	if node1 == nil || node2 == nil || node1.Val != node2.Val { return false }
+	return isSymmetricNode(node1.Left, node2.Right) && isSymmetricNode(node1.Right, node2.Left)
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSymmetricNode(root.Left, root.Right)
+}
