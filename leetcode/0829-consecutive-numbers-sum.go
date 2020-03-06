b9baba574207602270821557263d4b58d070dced
cc <chengchen.chn@outlook.com>
2020-03-06 17:27:02 +0800

    WIP: update leetcode 0829

---
+++ leetcode/0829-consecutive-numbers-sum.go
@@ -0,0 +1,39 @@
+package leetcode
+
+/**
+Given a positive integer N, how many ways can we write it as a sum of consecutive positive integers?
+
+Example 1:
+
+Input: 5
+Output: 2
+Explanation: 5 = 5 = 2 + 3
+Example 2:
+
+Input: 9
+Output: 3
+Explanation: 9 = 9 = 4 + 5 = 2 + 3 + 4
+Example 3:
+
+Input: 15
+Output: 4
+Explanation: 15 = 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
+Note: 1 <= N <= 10 ^ 9.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/consecutive-numbers-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// N = 1+2+3+4+......+k
+// O(N^(1/2))
+func consecutiveNumbersSum(N int) int {
+	res := 0
+	for i := 1; N > 0; i++ {
+		if N%i == 0 {
+			res++
+		}
+		N -= i
+	}
+	return res
+}
