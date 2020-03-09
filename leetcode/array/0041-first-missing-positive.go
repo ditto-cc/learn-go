49796ca37b1ed2dcb27df20c59e6c696f6864b72
cc <chengchen.chn@outlook.com>
2020-03-09 16:30:53 +0800

    WIP: update leetcode 41

---
+++ leetcode/array/0041-first-missing-positive.go
@@ -0,0 +1,41 @@
+package array
+
+/**
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/first-missing-positive
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		for 0 < nums[i] && nums[i] <= n && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		}
+	}
+
+	for i, e := range nums {
+		if e != i+1 {
+			return i + 1
+		}
+	}
+	return n + 1
+}
