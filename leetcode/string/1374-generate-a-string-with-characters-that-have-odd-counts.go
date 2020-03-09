bd3b1d0ef3ab42b99bbc9e84f539cb3d2d71260a
cc <chengchen.chn@outlook.com>
2020-03-10 01:15:24 +0800

    WIP: update leetcode 1374

---
+++ leetcode/string/1374-generate-a-string-with-characters-that-have-odd-counts.go
@@ -0,0 +1,47 @@
+package string
+
+/**
+Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+
+The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.
+
+
+
+Example 1:
+
+Input: n = 4
+Output: "pppz"
+Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+Example 2:
+
+Input: n = 2
+Output: "xy"
+Explanation: "xy" is a valid string since the characters 'x' and 'y' occur once. Note that there are many other valid strings such as "ag" and "ur".
+Example 3:
+
+Input: n = 7
+Output: "holasss"
+
+
+Constraints:
+
+1 <= n <= 500
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/generate-a-string-with-characters-that-have-odd-counts
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func generateTheString(n int) string {
+	if n&1 == 1 {
+		return genOutput("a", n)
+	}
+	return "b" + genOutput("a", n-1)
+}
+
+func genOutput(a string, n int) string {
+	res := ""
+	for ; n > 0; n-- {
+		res += a
+	}
+	return res
+}
