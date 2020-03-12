51a1df17c62267e4963a58c8a1ddb4444856c4d3
cc <chengchen.chn@outlook.com>
2020-03-12 22:20:20 +0800

    WIP: update leetcode 1266

---
+++ leetcode/array/1266-minimum-time-visiting-all-points.go
@@ -0,0 +1,61 @@
+package array
+
+/**
+On a plane there are n points with integer coordinates points[i] = [xi, yi]. Your task is to find the minimum time in seconds to visit all points.
+
+You can move according to the next rules:
+
+In one second always you can either move vertically, horizontally by one unit or diagonally (it means to move one unit vertically and one unit horizontally in one second).
+You have to visit the points in the same order as they appear in the array.
+ 
+
+Example 1:
+
+
+Input: points = [[1,1],[3,4],[-1,0]]
+Output: 7
+Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]   
+Time from [1,1] to [3,4] = 3 seconds 
+Time from [3,4] to [-1,0] = 4 seconds
+Total time = 7 seconds
+Example 2:
+
+Input: points = [[3,2],[-2,2]]
+Output: 5
+ 
+
+Constraints:
+
+points.length == n
+1 <= n <= 100
+points[i].length == 2
+-1000 <= points[i][0], points[i][1] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-time-visiting-all-points
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func minTimeToVisitAllPoints(points [][]int) int {
+	res, n := 0, len(points)
+	for i := 1; i < n; i++ {
+		pre, cur := points[i-1], points[i]
+		res += max(abs(cur[0] - pre[0]), abs(cur[1] - pre[1]))
+	}
+
+	return res
+}
