fd4c35b8c3d0637b55e26072f3243f6dd4433191
cc <chengchen.chn@outlook.com>
2020-03-13 14:10:25 +0800

    WIP: update leetcode 31

---
+++ leetcode/array/0031-next-permutation.go
@@ -0,0 +1,43 @@
+package array
+
+/**
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place and use only constant extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/next-permutation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for ; i >= 0 && nums[i] > nums[i+1]; i-- {
+
+	}
+
+	if i != -1 {
+		j := i + 1
+		for ; j < n && nums[j] > nums[i]; j++ {
+
+		}
+		nums[i], nums[j-1] = nums[j-1], nums[i]
+	}
+
+	reverse(nums, i+1, n-1)
+}
+
+func reverse(nums []int, l, r int) {
+	for i, j := l, r; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+}
