2ea989ccd044f858d1339cf5bf7a833a78e8e168
cc <chengchen.chn@outlook.com>
2020-03-16 23:29:19 +0800

    WIP: update leetcode 74

---
+++ leetcode/search/0074-Search-a-2D-Matrix.go
@@ -0,0 +1,49 @@
+package search
+
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-a-2d-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+
+	for i, j := len(matrix)-1, 0; i >= 0 && j < len(matrix[0]); {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			i--
+		} else [
+			j++
+		]
+	}
+	return false
+}
