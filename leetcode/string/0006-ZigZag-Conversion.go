05f2b15349bdd14b66b20665bc4797bd63cf4f65
cc <chengchen.chn@outlook.com>
2020-03-22 16:38:39 +0800

    WIP: update leetcode 3

---
+++ leetcode/string/0006-ZigZag-Conversion.go
@@ -0,0 +1,53 @@
+package string
+
+import (
+	"bytes"
+)
+
+/**
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+*/
+
+func convert(s string, numRows int) string {
+	n := len(s)
+	if numRows <= 1 || n <= 2 {
+		return s
+	}
+
+	pace, res := (numRows-1)<<1, bytes.Buffer{}
+	for i := 0; i < numRows; i++ {
+		for j := i; j < n; j += pace {
+			res.WriteByte(s[j])
+
+			index := j + pace - i*2
+			if i > 0 && index < n && j != index {
+				res.WriteByte(s[index])
+			}
+		}
+	}
+
+	return res.String()
+}
