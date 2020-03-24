500b1b0bb2bdc1d65b49cf933f0496d82802bb65
cc <chengchen.chn@outlook.com>
2020-03-24 11:29:18 +0800

    WIP: update offer 17.16

---
+++ algorithm/offer/17-16-The-Masseuse-LCCI.go
@@ -0,0 +1,52 @@
+package offer
+
+/**
+A popular masseuse receives a sequence of back-to-back appointment requests and is debating which ones to accept. She needs a break between appointments and therefore she cannot accept any adjacent requests. Given a sequence of back-to-back appoint­ ment requests, find the optimal (highest total booked minutes) set the masseuse can honor. Return the number of minutes.
+
+Note: This problem is slightly different from the original one in the book.
+
+
+
+Example 1:
+
+Input:  [1,2,3,1]
+Output:  4
+Explanation:  Accept request 1 and 3, total minutes = 1 + 3 = 4
+Example 2:
+
+Input:  [2,7,9,3,1]
+Output:  12
+Explanation:  Accept request 1, 3 and 5, total minutes = 2 + 9 + 1 = 12
+Example 3:
+
+Input:  [2,1,4,5,3,1,1,3]
+Output:  12
+Explanation:  Accept request 1, 3, 5 and 8, total minutes = 2 + 4 + 3 + 3 = 12
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/the-masseuse-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func massage(nums []int) int {
+	n := len(nums)
+	switch {
+	case 0 == n:
+		return 0
+	case 1 == n:
+		return nums[0]
+	case 2 < n:
+		nums[2] += nums[0]
+		for i := 3; i < n; i++ {
+			nums[i] += max(nums[i-2], nums[i-3])
+		}
+	}
+	return max(nums[n-1], nums[n-2])
+}
