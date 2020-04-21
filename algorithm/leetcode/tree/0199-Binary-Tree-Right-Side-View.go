0dd9453e1847ce24620ba90a2b8f955a59e58b7b
cc <chengchen.chn@outlook.com>
2020-04-22 00:24:58 +0800

    WIP: update leetcode 199

---
+++ algorithm/leetcode/tree/0199-Binary-Tree-Right-Side-View.go
@@ -0,0 +1,54 @@
+package tree
+
+/**
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+
+示例:
+
+输入: [1,2,3,null,5,null,4]
+输出: [1, 3, 4]
+解释:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rightSideView(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		size := len(q)
+		res = append(res, q[size-1].Val)
+
+		for i := 0; i < size; i++ {
+			node := q[i]
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		q = q[size:]
+	}
+
+	return res
+}
